7baebdccaca54b4000371e7f2b537c5855aa52d7
feyz <faizashrafkk@gmail.com>
2023-03-16 12:40:55 +0530

    userType and collegeId fetched along data

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.login(email, password);
     const token = createToken(user._id);
-    res.status(200).json({ email, token });
+    res.status(200).json({ email, token, userType, collegeId });
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
@@ -27,7 +27,7 @@ const signupUser = async (req, res) => {
     //create a token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ email, token, userType, collegeId });
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
